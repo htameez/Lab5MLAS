65d6b33af6cbcc8b4bc63064fce02c5aec627218
htameez <htameez@smu.edu>
2024-11-26 19:21:04 -0600

    FInished my tasks (connected frontend to backend).

---
+++ Lab5MLAS/HomeViewController.swift
@@ -0,0 +1,9 @@
+//
+//  HomeViewController.swift
+//  Lab5MLAS
+//
+//  Created by Hamna Tameez on 11/26/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import Foundation
---
+++ Lab5MLAS/QuizViewController.swift
@@ -0,0 +1,9 @@
+//
+//  QuizViewController.swift
+//  Lab5MLAS
+//
+//  Created by Hamna Tameez on 11/26/24.
+//  Copyright © 2024 Eric Larson. All rights reserved.
+//
+
+import Foundation
